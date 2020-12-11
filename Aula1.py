c91ddd8e450c98297438e1dbad0e632014f763c0
Leonardo Carvalho <44784555+Lccas@users.noreply.github.com>
2020-12-10 22:22:25 -0300

    Exemplos e exercicios da aula 1

--- Aula1.py
+++ Aula1.py
@@ -2,3 +2,17 @@ pais = ["Italia", "Japão", "Brasil"]
 quantidade = 4
 
 print(pais[1], "ganhou" ,quantidade ,"titulos mundiais")
+
+subst = "Python"
+verbo = "é"
+adjetivo = "fantástico"
+print(subst, verbo, adjetivo, sep="_", end="!\n")
+
+dia = 15
+mes = 10
+ano = 2015
+
+print(dia, mes, ano, sep="/")
+
+preco = 49.99
+print(type(preco))
