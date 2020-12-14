4eee1f02a2a73a9cd77ac800421d7700b7ec22ec
Leonardo Carvalho <44784555+Lccas@users.noreply.github.com>
2020-12-14 20:57:09 -0300

    implementação do while no codigo

--- jogos/Advinhação.py
+++ jogos/Advinhação.py
@@ -3,18 +3,26 @@ print("Seja bem vindo ao jogo de advinhação")
 print("************************************")
 
 numero_secreto = 42
-chute = int(input("Digite o seu numero: "))
+tentativas = 3
+rodada = 1
 
-print("Você digitou: ", chute)
+while (rodada <= tentativas):
+    print("Tentativa {} de {}".format(rodada, tentativas))
+    chute = int(input("Digite o seu numero: "))
+    print("Você digitou: ", chute)
 
-certo = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    certo = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if(certo):
-    print('Você acertou c:')
-else:
-    if(maior):
-        print('Você errou! O seu chute foi maior que o número secreto.')
-    elif(menor):
-        print('Você errou! O seu chute foi menor que o número secreto.')
+    if(certo):
+        print('Você acertou c:')
+    else:
+        if(maior):
+            print('Você errou! O seu chute foi maior que o número secreto.')
+        elif(menor):
+            print('Você errou! O seu chute foi menor que o número secreto.')
+
+    rodada = rodada + 1
+
+print("Fim do Jogo")
