5a6829c0a2cfb94a4b73f94fb8690a930d42f854
Leonardo Carvalho <44784555+Lccas@users.noreply.github.com>
2020-12-16 22:32:19 -0300

    implementação do for

--- jogos/Advinhação.py
+++ jogos/Advinhação.py
@@ -6,10 +6,14 @@ numero_secreto = 42
 tentativas = 3
 rodada = 1
 
-while (rodada <= tentativas):
+for rodada in range(1, tentativas + 1):
     print("Tentativa {} de {}".format(rodada, tentativas))
-    chute = int(input("Digite o seu numero: "))
+    chute = int(input("Digite um numero entre 1 e 100: "))
     print("Você digitou: ", chute)
+
+    if (chute < 1 or chute > 100):
+        print("Você deve inserir um número entre 1 e 100!")
+        continue
 
     certo = chute == numero_secreto
     maior = chute > numero_secreto
@@ -17,12 +21,11 @@ while (rodada <= tentativas):
 
     if(certo):
         print('Você acertou c:')
+        break
     else:
         if(maior):
             print('Você errou! O seu chute foi maior que o número secreto.')
         elif(menor):
             print('Você errou! O seu chute foi menor que o número secreto.')
-
-    rodada = rodada + 1
 
 print("Fim do Jogo")
