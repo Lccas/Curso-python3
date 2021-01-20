f154b90fe1f689ff0534a32857eb89900c75d45b
Leonardo Carvalho <44784555+Lccas@users.noreply.github.com>
2021-01-19 22:03:42 -0300

    aplica dificuldades no jogo

--- jogos/Advinhação.py
+++ jogos/Advinhação.py
@@ -6,8 +6,19 @@ print("Seja bem vindo ao jogo de advinhação")
 print("************************************")
 
 numero_secreto = random.randrange(1, 101)
-tentativas = 3
+tentativas = 0
 
+print("Qual nivel de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Define o nível: "))
+
+if (nivel == 1):
+    tentativas = 20
+elif (nivel == 2):
+    tentativas = 10
+else :
+    tentativas = 5
 
 for rodada in range(1, tentativas + 1):
     print("Tentativa {} de {}".format(rodada, tentativas))
