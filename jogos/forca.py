c407a42e218f3db7985aff05b9b69eb6d7ed66ba
Leonardo Carvalho <44784555+Lccas@users.noreply.github.com>
2021-02-01 19:52:04 -0300

    aula 8

---
+++ jogos/forca.py
@@ -0,0 +1,9 @@
+def jogar():
+    print("************************************")
+    print("Seja bem vindo ao jogo de Forca ")
+    print("************************************")
+
+    print("Fim do Jogo")
+
+if(__name__ == "__main__"):
+    jogar()
