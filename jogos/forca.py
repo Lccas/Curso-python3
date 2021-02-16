c42fbcb720d8f53e6379f6d6b552670fe5e28c93
Leonardo Carvalho <44784555+Lccas@users.noreply.github.com>
2021-02-15 21:54:51 -0300

    jogo de forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,7 +1,15 @@
 def jogar():
     print("************************************")
-    print("Seja bem vindo ao jogo de Forca ")
+    print("**Seja bem vindo ao jogo de Forca **")
     print("************************************")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not acertou and not enforcou):
+        print("jogando...")
 
     print("Fim do Jogo")
 
